4323d57b666399f356a200920f450ffef92f9dc8
RCOak <108288043+RCOak@users.noreply.github.com>
2022-12-22 23:49:19 +0000

    Estrutura

--- src/App.js
+++ src/App.js
@@ -2,21 +2,24 @@ import { BrowserRouter as Router, Route, Routes, Link  } from "react-router-dom"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 
+import Container from "./layout/Container";
+
 function App() {
   return (
     <div>
       <Router>
-
-<div>
-  <Link to='/'>Home</Link>
-  <Link to='/login'>Login</Link>
-</div>
-<Routes>
-  <Route exact path='/' element={<Home />} />
-  <Route exact path='/login' element={<Login />} />
-</Routes>
-<footer>Footer</footer>
-</Router>
+        <div>
+          <Link to='/'>Home</Link>
+          <Link to='/login'>Login</Link>
+        </div>
+        <Container customClass="minHeight">
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/login' element={<Login />} />
+          </Routes>
+        </Container>
+        <p>Footer</p>
+        </Router>
     </div>
   );
 }
