2e1d1ec6c09c2f2b6d541af7bebac83487a3f232
RCOak <108288043+RCOak@users.noreply.github.com>
2022-12-28 22:47:51 +0000

    img

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import React from "react";
 
 import Home from "./Components/pages/Home.js";
 import Login from "./Components/pages/Login.js";
+import Cart from "./Components/pages/Cart.js";
 
 import Container from "./Components/layout/Container.js";
-import Navbar from "./Components/layout/Navbar.jsx";
+import Navbar from "./Components/layout/Navbar.jsx"
 import Footer from "./Components/layout/Footer.jsx";
-
 
 function App() {
   return (
@@ -18,6 +18,7 @@ function App() {
             <Routes>
               <Route exact path='/' element={<Home />} />
               <Route exact path='/login' element={<Login />} />
+              <Route exact path='/cart' element={<Cart />} />
             </Routes>
           </Container>
         <Footer />
--- src/Components/layout/Navbar.jsx
+++ src/Components/layout/Navbar.jsx
@@ -3,21 +3,48 @@ import React from 'react'
 import Container from './Container.js'
 
 import styles from './Navbar.module.css'
+import { BsFillPersonFill, BsBag, BsList, BsFillGeoAltFill, BsFillTelephoneFill, BsEnvelopeFill, BsSearch } from "react-icons/bs";
 
 function Navbar() {
   return(
-        <nav className={styles.navbar}>
-          <Container>
-            <ul className={styles.list}>
-              <li className={styles.item}>
-                <Link to='/'>Home</Link>
-              </li>
-              <li className={styles.item}>
-                <Link to='/login'>Login</Link>
-              </li>
-            </ul>           
-          </Container>          
-        </nav>
+        <div>
+          <div className={styles.unavbar}>
+                <ul className={styles.list}>
+                  <li className={styles.item}>
+                  <span className={styles.icons}><BsFillGeoAltFill /></span><p>endereço</p>
+                  </li>
+                  <li className={styles.item}>
+                  <span className={styles.icons}><BsFillTelephoneFill /></span><p>telefone</p>
+                  </li>
+                  <li className={styles.item}>
+                  <span className={styles.icons}><BsEnvelopeFill /></span><p>email</p>
+                  </li>
+                  <li className={styles.itemb}>
+                  <p><BsSearch /></p>
+                  </li>
+                </ul>           
+          </div>
+          <nav className={styles.navbar}>
+            <Container>
+              <div className={styles.logo}>
+                <p>
+                  <Link to='/'><img src='https://cdn.discordapp.com/attachments/596820253984555164/1057348548716408935/Dado-Vorpal-Marca.png' alt=''></img></Link>
+                </p>
+              </div>
+              <div className={styles.menuicons}>
+                <p className={styles.item}>
+                  <Link to='/login'><p className={styles.ilogin}><BsFillPersonFill /></p></Link>
+                </p>
+                <p className={styles.item}>
+                  <Link to='/Cart'><p className={styles.cart}><BsBag /></p></Link>
+                </p>
+                <p className={styles.item}>
+                  <p className={styles.menu}><BsList /></p>
+                </p>           
+              </div>
+            </Container>          
+          </nav>
+        </div>
   )
 }
 
